d6e6623cfac05682d5df07d49f2427a23f15d51a
Sid <sidvekariya20@gmail.com>
2023-08-23 15:17:56 +0530

    make an array in utils

---
+++ utils/massageData.js
@@ -0,0 +1,29 @@
+export const massageData = [
+    {
+        id: 1,
+        massageName: "Swedish Deep Tissue Massage",
+        price: 10000,
+        massageNumber: "SPA.01",
+        media: {
+            thumbnail: require("@/public/images/spa1.png"),
+        },
+    },
+    {
+        id: 2,
+        massageName: "Facials Massage with Scrubs",
+        price: 8000,
+        massageNumber: "SPA.02",
+        media: {
+            thumbnail: require("@/public/images/spa2.png"),
+        },
+    },
+    {
+        id: 3,
+        massageName: "Body Treatments with Scrubs",
+        price: 12000,
+        massageNumber: "SPA.03",
+        media: {
+            thumbnail: require("@/public/images/spa3.png"),
+        },
+    },
+];
