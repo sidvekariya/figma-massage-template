3769dedf9671bce84652a5c425eedd06b95fe233
Sid <sidvekariya20@gmail.com>
2023-08-24 10:36:38 +0530

    array updated

--- utils/massageData.js
+++ utils/massageData.js
@@ -4,26 +4,23 @@ export const massageData = [
         massageName: "Swedish Deep Tissue Massage",
         price: 10000,
         massageNumber: "SPA.01",
-        media: {
-            thumbnail: require("@/public/images/spa1.png"),
-        },
+        line: "/images/line.png",
+        media: "/images/spa1.png",
     },
     {
         id: 2,
         massageName: "Facials Massage with Scrubs",
         price: 8000,
         massageNumber: "SPA.02",
-        media: {
-            thumbnail: require("@/public/images/spa2.png"),
-        },
+        media: "/images/spa2.png",
+        line: "/images/line.png",
     },
     {
         id: 3,
         massageName: "Body Treatments with Scrubs",
         price: 12000,
         massageNumber: "SPA.03",
-        media: {
-            thumbnail: require("@/public/images/spa3.png"),
-        },
+        line: "/images/line.png",
+        media: "/images/spa3.png",
     },
 ];
