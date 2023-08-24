831e8a88113e77e7188632284efc3d681f187f95
Sid <sidvekariya20@gmail.com>
2023-08-24 17:00:07 +0530

    hero section added in homepage

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MassageCardList from "./ItemComponent";
+import Hero from "./Hero";
 
 const HomePage = () => {
     return (
         <div className="app">
+            <Hero />
             <MassageCardList />
         </div>
     );
