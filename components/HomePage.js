38f03207e4a87b87798ceac096503303ba8a0148
Sid <sidvekariya20@gmail.com>
2023-08-24 10:44:09 +0530

    component added in app.js

---
+++ components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MassageCardList from "./ItemComponent";
+
+const HomePage = () => {
+    return (
+        <div className="app">
+            <MassageCardList />
+        </div>
+    );
+};
+
+export default HomePage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import HomePage from "@/components/HomePage";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <HomePage />
+            <Component {...pageProps} />
+        </div>
+    );
 }
