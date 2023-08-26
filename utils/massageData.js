05dd8f7d80dc8e284a3261d6d7838a0e20c65b4c
Sid <sidvekariya20@gmail.com>
2023-08-26 16:12:08 +0530

    price changes in data

--- utils/massageData.js
+++ utils/massageData.js
@@ -2,7 +2,7 @@ export const massageData = [
     {
         id: 1,
         massageName: "Swedish Deep Tissue Massage",
-        price: 10000,
+        price: 55000000,
         massageNumber: "SPA.01",
         line: "/images/line.png",
         media: "/images/spa1.png",
@@ -10,7 +10,7 @@ export const massageData = [
     {
         id: 2,
         massageName: "Facials Massage with Scrubs",
-        price: 8000,
+        price: 95000000,
         massageNumber: "SPA.02",
         media: "/images/spa2.png",
         line: "/images/line.png",
@@ -18,7 +18,7 @@ export const massageData = [
     {
         id: 3,
         massageName: "Body Treatments with Scrubs",
-        price: 12000,
+        price: 65000000,
         massageNumber: "SPA.03",
         line: "/images/line.png",
         media: "/images/spa3.png",
